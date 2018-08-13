cab3f00def0a3428249c6d69ac603e3c1a6da2e1
dipu-bd <dipu.sudipta@gmail.com>
2018-08-13 12:46:33 +0600

    Refactor ebook generation

--- ebook_crawler/binding.py
+++ ebook_crawler/binding.py
@@ -19,6 +19,41 @@ KINDLEGEN_PATH_LINUX = os.path.join(DIR_NAME, 'ext', 'kindlegen-linux')
 KINDLEGEN_PATH_WINDOWS = os.path.join(DIR_NAME, 'ext', 'kindlegen-windows')
 SYSTEM_OS = platform.system()
 
+
+def get_epub_file(book_title, volume_no):
+    '''returns the epub output file'''
+    output_path = os.path.join(book_title, 'epub')
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # end if
+    file_name = book_title + '_v' + volume_no + '.epub'
+    file_path = os.path.join(output_path, file_name)
+    return file_path
+# end def
+
+def get_mobi_file(book_title, volume_no):
+    '''returns the epub output file'''
+    output_path = os.path.join(book_title, 'mobi')
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # end if
+    file_name = book_title + '_v' + volume_no + '.mobi'
+    file_path = os.path.join(output_path, file_name)
+    return file_path
+# end def
+
+def get_text_file(book_title, volume_no, chapter_no):
+    '''returns the epub output file'''
+    output_path = os.path.join(book_title, 'text')
+    output_path = os.path.join(output_path, book_title + '_v' + volume_no)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # end if
+    file_name = chapter_no + '.txt'
+    file_path = os.path.join(output_path, file_name)
+    return file_path
+# end def
+
 def novel_to_kindle(input_path):
     ''''Convert novel to epub'''
     if not os.path.exists(input_path):
@@ -27,6 +62,10 @@ def novel_to_kindle(input_path):
     novel_id = os.path.basename(input_path)
     # Create epubs by volumes
     for volume_no in sorted(os.listdir(input_path)):
+        full_vol = os.path.join(input_path, volume_no)
+        if not os.path.isdir(full_vol):
+            continue
+        # end if
         try:
             # create book
             book = epub.EpubBook()
@@ -37,7 +76,6 @@ def novel_to_kindle(input_path):
             book_title = None
             book_author = None
             vol = volume_no.rjust(2, '0')
-            full_vol = os.path.join(input_path, volume_no)
             print('Processing:', full_vol)
             for file_name in sorted(os.listdir(full_vol)):
                 # read data
@@ -64,48 +102,48 @@ def novel_to_kindle(input_path):
             book.toc = contents
             book.add_item(epub.EpubNav())
             book.add_item(epub.EpubNcx())
-            # Create epub
+            # Create epub file
             book_title = re.sub('[\\\\/*?:"<>|]' or r'[\\/*?:"<>|]',"", book_title)
-            output_path = book_title
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            # end if
-            file_name = book_title + '_v' + volume_no + '.epub'
-            file_path = os.path.join(output_path, file_name)
-            print('Creating:', file_path)
-            epub.write_epub(file_path, book, {})
+            epub_path = get_epub_file(book_title, volume_no)
+            print('Creating:', epub_path)
+            epub.write_epub(epub_path, book, {})
+            # convert epub to mobi
+            mobi_path = get_mobi_file(book_title, volume_no)
+            epub_to_mobi(epub_path, mobi_path)
         except:
             pass
         # end try
     # end for
+# end def
 
-    # Convert to mobi format
-    for file_name in sorted(os.listdir(output_path)):
-        if not file_name.endswith('.epub'):
-            continue
-        epub_file = os.path.join(output_path, file_name)
-        if SYSTEM_OS == 'Linux':
-            call([KINDLEGEN_PATH_LINUX, epub_file])
-        elif SYSTEM_OS == 'Darwin':
-            call([KINDLEGEN_PATH_MAC, epub_file])
-        elif SYSTEM_OS == 'Windows':
-            call([KINDLEGEN_PATH_WINDOWS, epub_file])
-        else:
-            print('KindleGen does not support this OS.')
-            break
+def epub_to_mobi(epub_file, mobi_file):
+    ''''Convert epub to mobi'''
+    if not epub_file.endswith('.epub'):
+        return
+    # end if
+    # Mobi file generator
+    generator = lambda gen: call([gen, '-c2', epub_file])
+    # Try using system kindlegen
+    try:
+        return generator('kindlegen')
+    except (OSError, Exception) as err:
+        if err[1].errno == errno.ENOENT:
+            print("Warning: kindlegen was not on your path; not generating .mobi version")
+            print("Visit https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000765211 to install it.")
         # end if
-        # Testing alternatives
-        # try:
-        #     call(['kindlegen', '-c2', epub_file])
-        # except (OSError, Exception) as err:
-        #     if err[1].errno == errno.ENOENT:
-        #         print("Warning: kindlegen was not on your path; not generating .mobi version")
-        #         print("Visit https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000765211 to install it.")
-        #     else:
-        #         raise
-        #     # end if
-        # # end try
-    # end for
+        pass
+    # end try
+    # Use local kindlegen
+    if SYSTEM_OS == 'Linux':
+        return generator(KINDLEGEN_PATH_LINUX)
+    elif SYSTEM_OS == 'Darwin':
+        return generator(KINDLEGEN_PATH_MAC)
+    elif SYSTEM_OS == 'Windows':
+        return generator(KINDLEGEN_PATH_WINDOWS)
+    else:
+        print("Warning: kindlegen does not support your OS; not generating .mobi version")
+        print("Visit https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000765211 to install it.")
+    # end if
 # end def
 
 
