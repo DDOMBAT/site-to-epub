0c117b59764529e446b8192554953af8e45c8d58
dipu-bd <dipu.sudipta@gmail.com>
2018-08-21 22:45:13 +0600

    Update method: crawlChapters

--- lib/interfaces/crawler.ts
+++ lib/interfaces/crawler.ts
@@ -9,6 +9,7 @@ export interface Crawler {
    * Returns the current status of the crawler.
    */
   status? (): string
+
   /**
    * Returns the current progress of the crawler.
    */
@@ -19,6 +20,7 @@ export interface Crawler {
    * @param link the url
    */
   canCrawl (link: string): boolean
+
   /**
    * Parse the novel id from a given url. It must be crawlable by the current
    * crawler. Otherwise `null` will be returned.
@@ -31,6 +33,7 @@ export interface Crawler {
    * @param query the novel query
    */
   searchNovels (query: string): Promise<SearchResult[]>
+
   /**
    * Get more search results.
    * The query parameter is optional. If provided, it will be matched against
@@ -44,10 +47,19 @@ export interface Crawler {
    * @param novelId the novel id
    */
   getNovel (novelId: string): Promise<NovelInfo>
+
   /**
    * Crawl chapters of a novel. It only crawls chapters with empty `body`. Pass
    * `true` to the restart parameter to start crawling from the beginning.
    * @param novel the novel info to crawl
+   * @param start the starting chapter
+   * @param stop the final chapter
+   * @param restart to redownlod already available chapters
    */
-  crawlChapters (novel: NovelInfo, restart?: boolean): Promise<NovelInfo>
+  crawlChapters (
+    novel: NovelInfo,
+    start?: string | number,
+    stop?: string | number,
+    restart?: boolean
+  ): Promise<NovelInfo>
 }
