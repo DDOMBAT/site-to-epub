e4f7a418ce8221136f559c9c5661dcf929cc56e2
dipu-bd <dipu.sudipta@gmail.com>
2018-08-21 22:38:32 +0600

    Update crawler.ts: add more methods

--- lib/interfaces/crawler.ts
+++ lib/interfaces/crawler.ts
@@ -1,6 +1,53 @@
 import { SearchResult } from './search-result'
+import { NovelInfo } from './novel-info'
 
 export interface Crawler {
+  /// The name of the crawler.
   readonly name?: string
-  searchNovels (name: string): SearchResult
+
+  /**
+   * Returns the current status of the crawler.
+   */
+  status? (): string
+  /**
+   * Returns the current progress of the crawler.
+   */
+  progress? (): number
+
+  /**
+   * Returns true if the crawler can crawl the given url.
+   * @param link the url
+   */
+  canCrawl (link: string): boolean
+  /**
+   * Parse the novel id from a given url. It must be crawlable by the current
+   * crawler. Otherwise `null` will be returned.
+   * @param link the given url
+   */
+  getNovelId (link: string): string | null
+
+  /**
+   * Search for novels.
+   * @param query the novel query
+   */
+  searchNovels (query: string): Promise<SearchResult[]>
+  /**
+   * Get more search results.
+   * The query parameter is optional. If provided, it will be matched against
+   * the ongoing query. Empty result will be returned, if not matched.
+   * @param query
+   */
+  searchNovelsNext? (query?: string): Promise<SearchResult[]>
+
+  /**
+   * Get top level informations about a novel.
+   * @param novelId the novel id
+   */
+  getNovel (novelId: string): Promise<NovelInfo>
+  /**
+   * Crawl chapters of a novel. It only crawls chapters with empty `body`. Pass
+   * `true` to the restart parameter to start crawling from the beginning.
+   * @param novel the novel info to crawl
+   */
+  crawlChapters (novel: NovelInfo, restart?: boolean): Promise<NovelInfo>
 }
